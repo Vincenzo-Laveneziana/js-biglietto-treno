ed6260c8a9f7502a70b95a25e37c467c48d5bb6d
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-03-26 15:56:55 +0100

    Fine js, inizio stesura html e css

--- js/main.js
+++ js/main.js
@@ -7,29 +7,41 @@
 //Chiediamo il numero di Km
 
 var numeroKm = parseInt(prompt("Quanti Km devi percorrere?"));
-console.log("Km da percorrere " , numeroKm + ".");
+console.log("Km da percorrere " + numeroKm + ".");
 
 
 //Chiediamo età passeggero
 var age = parseInt(prompt("Qual'è la tua età"));
-console.log("Età del passeggero " , age + ".");
+console.log("Età del passeggero " + age + ".");
 
 
 //dichiariamo prezzo costo biglietto 
 var costoKm = 0.21;
-console.log(costoKm);
+console.log("Costo per km = " + costoKm + "€");
 
 var prezzoTot = numeroKm * costoKm;
-console.log(prezzoTot);
+console.log("Prezzo del biglietto = " + prezzoTot + "€");
 
 //sconto del 20%
 var sconto20 = prezzoTot * 20 / 100;
 var prezzoFinaleSconto20 = prezzoTot - sconto20;
-console.log("Prezzo sconto 20% " , sconto20);
-console.log("Prezzo Finale con sconto " , prezzoFinaleSconto20);
 
 //sconto del 40%
 var sconto40 = prezzoTot * 40 / 100;
 var prezzoFinaleSconto40 = prezzoTot - sconto40;
-console.log("Prezzo sconto 40% " , sconto40);
-console.log("Prezzo Finale con sconto " , prezzoFinaleSconto40);
+
+
+//applicazione degli sconti
+
+var minorenni = 18;
+var over = 65;
+
+if (age < 18){
+  console.log("Prezzo sconto 20% = " , sconto20.toFixed(2) + "€");
+  console.log("Prezzo Finale con sconto 20% = " + prezzoFinaleSconto20.toFixed(2) + "€");
+}else if (age > 65){
+  console.log("Prezzo sconto 40% = " , sconto40.toFixed(2) + "€");
+  console.log("Prezzo Finale con sconto 40% = " + prezzoFinaleSconto40.toFixed(2) + "€");
+}else {
+  console.log("Mi dispiace se sei sfigato, ma devi pagare il biglietto per Intero ^_^")
+}
