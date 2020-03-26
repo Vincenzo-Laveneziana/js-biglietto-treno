1004a407b293b16a5534f1bfa99970a5f6b08951
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-03-26 17:25:05 +0100

    Stesura html and css, integrazione js

--- js/main.js
+++ js/main.js
@@ -8,19 +8,22 @@
 
 var numeroKm = parseInt(prompt("Quanti Km devi percorrere?"));
 console.log("Km da percorrere " + numeroKm + ".");
+document.getElementById("numeroKm").innerHTML = "Km da percorrere " + numeroKm;
 
 
 //Chiediamo età passeggero
 var age = parseInt(prompt("Qual'è la tua età"));
 console.log("Età del passeggero " + age + ".");
+document.getElementById("age").innerHTML = "Età passeggero " + age;
 
 
-//dichiariamo prezzo costo biglietto 
+//dichiariamo prezzo costo biglietto
 var costoKm = 0.21;
 console.log("Costo per km = " + costoKm + "€");
 
 var prezzoTot = numeroKm * costoKm;
 console.log("Prezzo del biglietto = " + prezzoTot + "€");
+document.getElementById("prezzoTot").innerHTML = "Prezzo del biglietto = " + prezzoTot + "€"
 
 //sconto del 20%
 var sconto20 = prezzoTot * 20 / 100;
@@ -38,10 +41,19 @@ var over = 65;
 
 if (age < 18){
   console.log("Prezzo sconto 20% = " , sconto20.toFixed(2) + "€");
+  document.getElementById("sconto").innerHTML = "Prezzo sconto 20% = " + sconto20 + "€"
+
   console.log("Prezzo Finale con sconto 20% = " + prezzoFinaleSconto20.toFixed(2) + "€");
+  document.getElementById("prezzoFinaleSconto").innerHTML = "Prezzo Finale con sconto 20% = " + prezzoFinaleSconto20 + "€"
+ 
 }else if (age > 65){
   console.log("Prezzo sconto 40% = " , sconto40.toFixed(2) + "€");
+  document.getElementById("sconto").innerHTML = "Prezzo sconto 40% = " + sconto40 + "€"
+
   console.log("Prezzo Finale con sconto 40% = " + prezzoFinaleSconto40.toFixed(2) + "€");
+  document.getElementById("prezzoFinaleSconto").innerHTML = "Prezzo Finale con sconto 40% = " + prezzoFinaleSconto40 + "€"
 }else {
   console.log("Mi dispiace se sei sfigato, ma devi pagare il biglietto per Intero ^_^")
+  document.getElementById("sconto").innerHTML = "Mi dispiace se sei sfigato, ma devi pagare il biglietto per Intero ^_^"
+  document.getElementById("prezzoFinaleSconto").innerHTML = "Prezzo finale del biglietto = " + prezzoTot + "€"
 }
